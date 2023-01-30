b35f45bb5fdef78fcd88013b8453d8652a3e95bf
doanything28 <doanything8899@gmail.com>
2023-01-30 16:55:30 +0530

    some changes:

--- server/app.js
+++ server/app.js
@@ -4,19 +4,20 @@ require("dotenv").config();
 
 // internal packages (modules)
 const connectToMongo = require("./database/db_connect");
+const route1 = require("./routes/route1");
 
 // app code start
 const app = express();
+
 // atlas string in .env file
 // const url = process.env.MONGO_ATLAS_URI;
 const url = "mongodb://localhost:27017/finalProjectDB";
 const port = process.env.PORT || 5200;
 const hostname = "localhost";
+app.use(express.json());
 
 // All routes
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use("/auth", route1);
 // start arrow function that connect to db and listen the port
 const start = async () => {
   try {
